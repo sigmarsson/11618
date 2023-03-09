5ad3b0e162fdb7932c05d6d2d01da54ba277fcbb
Tommy Gombar <weather.history@outlook.com>
2023-03-09 13:06:34 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,67 @@
+using Microsoft.UI.Xaml;
+
+using Weather.History.Log;
+using Weather.History.CrossCutting;
+
+using System.IO;
+using System;
+
+namespace Weather.History
+{
+    public sealed partial class App
+    {
+        public App()
+        {
+#if WINDOWS
+            UnhandledException += ExceptionHandler;
+#endif
+            Environment.SetEnvironmentVariable(MyEnvironment.VARIABLE, AppDataLocation.Current, EnvironmentVariableTarget.Process);
+
+            var logDirectory = Path.Combine(AppDataLocation.Current, "Log");
+
+            Directory.CreateDirectory(logDirectory);
+
+            Log4.Debug($"Current Working Directory : {SetCwd()}");
+            Log4.Debug($"Log Directory: {logDirectory}");
+            Log4.Info($"Time zone: {TimeZoneInfo.Local.Id}");
+
+            InitializeComponent();
+        }
+
+        private string SetCwd()
+        {
+            try
+            {
+                var exe = GetType().Assembly.Location;
+                var cwd = Path.GetDirectoryName(exe);
+
+                Directory.SetCurrentDirectory(cwd);
+
+                return cwd;
+            }
+            catch (Exception e)
+            {
+                Log4.Error(e);
+
+                return null;
+            }
+        }
+
+#if WINDOWS
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            base.OnLaunched(e);
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+        }
+#endif
+        public void ExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            Log4.Error(e.Exception);
+        }
+    }
+}
---
+++ App1/App1/App.Prism.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+using Microsoft.UI.Xaml;
+
+
+
+#if WINDOWS
+using System.Diagnostics;
+
+using Weather.History.Windows;
+
+using Windows.ApplicationModel;
+#endif
+
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using Prism.DryIoc;
+
+
+namespace Weather.History
+{
+    public sealed partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        
+        }
+
+
+
+        protected override UIElement CreateShell()
+        {
+#if WINDOWS
+            shell.Loaded += (s, e) =>
+            {
+                MainXamlRoot = (s as UIElement).XamlRoot;
+            };
+#endif
+            return null;
+        }
+
+#if WINDOWS
+        protected override void InitializeShell(UIElement shell)
+        {
+
+            Window = new Window
+            {
+                ExtendsContentIntoTitleBar = true
+            };
+
+            Window.Activate();
+            Window.Content = shell;
+			
+			Win32Ex.UpdateTitleBar(Window.Content);
+
+        }
+#endif
+
+        protected override void OnInitialized()
+        {
+#if WINDOWS
+            var proc = Process.GetCurrentProcess();
+            var hwnd = proc.MainWindowHandle;
+            var geoService = Container.Resolve<IGeoService>();
+            var appService = new AppServiceHost(hwnd, geoService);
+
+            appService.Start();
+#endif
+        }
+
+        protected override void ConfigureRegionAdapterMappings(RegionAdapterMappings regionAdapterMappings)
+        {
+            base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+        }
+    }
+}
